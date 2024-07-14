f9bc1048f78c4e73eb6ce2d36dac4c979ed65cf9
Johannes Konings <johannes.konings@outlook.com>
2024-07-14 19:49:22 +0200

    update

--- .projenrc.ts
+++ .projenrc.ts
@@ -12,6 +12,16 @@ const foo = {
   baz: "hello",
 };
 
+new JsonFile(project, "foo1.json", {
+  readonly: true,
+  marker: true,
+  obj: foo,
+});
+new JsonFile(project, "foo2.json", {
+  readonly: true,
+  marker: false,
+  obj: foo,
+});
 new JsonFile(project, "foo3.json", {
   readonly: false,
   marker: false,
